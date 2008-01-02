ae3870d228781a046f875c7c3ea1137586d47bcb
Sundar Dorai-Raj <sundar.dorai-raj@pdf.com>
2008-01-02 00:00:00 +0000

    version 1.0-3

--- R/binom.bayes.R
+++ R/binom.bayes.R
@@ -16,6 +16,7 @@ binom.bayes <- function(x, n, conf.level = 0.95, type = c("highest", "central"),
   lcl <- qbeta(alpha, a, b)
   ucl <- qbeta(1 - alpha, a, b)
   hpd <- any(pmatch(type, "highest", nomatch = FALSE))
+  error <- vector("integer", length(lcl))
   if(any(!ends) && hpd) {
     ci <- .C("binom_bayes",
              as.integer(x[!ends]),
@@ -28,15 +29,25 @@ binom.bayes <- function(x, n, conf.level = 0.95, type = c("highest", "central"),
              as.integer(sum(!ends)),
              maxit = as.integer(maxit),
              tol = as.double(tol),
+             error = error[!ends],
              PACKAGE = "binom")
     lcl[!ends] <- ci[["lcl"]]
     ucl[!ends] <- ci[["ucl"]]
+    if(any(ci$error)) {
+      nerr <- sum(ci$error)
+      msg1 <- sprintf("%s confidence interval%s ", nerr, if(nerr > 1) "s" else "")
+      msg2 <- "failed to converge (marked by '*').\n"
+      msg3 <- "  Try changing 'tol' to a different value."
+      warning(msg1, msg2, msg3)
+    }
   }
   lcl[x == 0] <- 0
   ucl[x == n] <- 1
   sig <- pbeta(lcl, a, b) + 1 - pbeta(ucl, a, b)
   res <- data.frame(method = "bayes", x = x, n = n, shape1 = a, shape2 = b,
                     mean = p, lower = lcl, upper = ucl, sig = sig)
+  if(any(ci$error))
+    res$method <- factor(sprintf("bayes%s", ifelse(ci$error, "*", "")))
   attr(res, "conf.level") <- conf.level
   res
 }
